f7366c039f8f29f2bb34d2cfee5fcdd014493989
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-01-09 11:28:57 +0700

    Update RegisterFormView.php

--- RegisterFormView.php
+++ RegisterFormView.php
@@ -5,10 +5,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  <?php $this->load->view('css'); ?>
 </head>
 <body>
   <div class="container">
@@ -39,7 +35,6 @@
 </body>
 </html>
 
-<?php $this->load->view('script'); ?>
 <script type="text/javascript">
 
   function checkEmail(str){

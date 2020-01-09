75469ea605264701e26e362b7f0a70f0c3928ed2
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-01-09 11:32:37 +0700

    Update RegisterFormView.php

--- RegisterFormView.php
+++ RegisterFormView.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title><?php echo $this->lang->line("แบบฟอร์มลงทะเบียน");?></title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-</head>
-<body>
-  dddddddddddddddddddddddddddddd
-  </body>
-</html>
-
+<?php
+echo "ddddd";
+?>
